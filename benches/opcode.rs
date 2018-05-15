ed552122beafc18226cf7e36224ae23af78f27ba
Konrad Borowski <github@borowski.pw>
2018-05-15 10:03:12 +0200

    Benchmark identifier match

--- benches/opcode.rs
+++ benches/opcode.rs
@@ -7,6 +7,11 @@ use mvp::parser::grammar::{self, CompleteStr};
 use test::Bencher;
 
 #[bench]
+fn identifier(b: &mut Bencher) {
+    b.iter(|| grammar::identifier(CompleteStr("LDA")));
+}
+
+#[bench]
 fn address(b: &mut Bencher) {
     b.iter(|| grammar::statement(CompleteStr("LDA $19")));
 }
